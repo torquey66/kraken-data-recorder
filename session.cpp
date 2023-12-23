61277a43b609d00b0d3aebe66803d6a8ed55138f
John Finley <torquey@gmail.com>
2023-12-23 14:10:28 -0600

    Improve heartbeat scheme

--- session.cpp
+++ session.cpp
@@ -1,8 +1,5 @@
 #include "session.hpp"
 
-#include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
-#include <boost/beast/websocket/ssl.hpp>
 #include <boost/log/trivial.hpp>
 #include <nlohmann/json.hpp>
 
@@ -17,62 +14,63 @@ void fail(boost::beast::error_code ec, char const *what) {
 namespace krakpot {
 
 session_t::session_t(ioc_t &ioc, ssl_context_t &ssl_context)
-    : m_ioc(ioc), m_ssl_context(ssl_context) {}
+    : m_ioc{ioc}, m_ssl_context{ssl_context}, m_ws{ioc, ssl_context} {
 
-void session_t::start_heartbeat(std::string host, std::string port) {
   boost::asio::spawn(
       m_ioc,
-      [this, host, port](yield_context_t yc) {
-        this->run_heartbeat(host, port, yc);
+      [this](yield_context_t yc) {
+        handshake("ws.kraken.com", "443", yc);
+        run_heartbeat(yc);
       },
       [](std::exception_ptr ex) {
-        BOOST_LOG_TRIVIAL(info) << "Mooooooooooooooooooooooo!";
         if (ex)
           std::rethrow_exception(ex);
       });
 }
 
-void session_t::run_heartbeat(std::string host, std::string port,
-                              yield_context_t yield) {
+void session_t::handshake(std::string host, std::string port,
+                          yield_context_t yield) {
 
   boost::beast::error_code ec;
 
   boost::asio::ip::tcp::resolver resolver(m_ioc);
-  boost::beast::websocket::stream<
-      boost::beast::ssl_stream<boost::beast::tcp_stream>>
-      ws(m_ioc, m_ssl_context);
   auto const results = resolver.async_resolve(host, port, yield[ec]);
   if (ec)
     return fail(ec, "resolve");
-  boost::beast::get_lowest_layer(ws).expires_after(std::chrono::seconds(30));
+  boost::beast::get_lowest_layer(m_ws).expires_after(std::chrono::seconds(30));
   auto ep =
-      boost::beast::get_lowest_layer(ws).async_connect(results, yield[ec]);
+      boost::beast::get_lowest_layer(m_ws).async_connect(results, yield[ec]);
   if (ec)
     return fail(ec, "connect");
-  if (!SSL_set_tlsext_host_name(ws.next_layer().native_handle(),
+  if (!SSL_set_tlsext_host_name(m_ws.next_layer().native_handle(),
                                 host.c_str())) {
     ec = boost::beast::error_code(static_cast<int>(::ERR_get_error()),
                                   boost::asio::error::get_ssl_category());
     return fail(ec, "connect");
   }
   host += ':' + std::to_string(ep.port());
-  boost::beast::get_lowest_layer(ws).expires_after(std::chrono::seconds(30));
-  ws.set_option(boost::beast::websocket::stream_base::decorator(
+  boost::beast::get_lowest_layer(m_ws).expires_after(std::chrono::seconds(30));
+  m_ws.set_option(boost::beast::websocket::stream_base::decorator(
       [](boost::beast::websocket::request_type &req) {
         req.set(boost::beast::http::field::user_agent,
                 std::string(BOOST_BEAST_VERSION_STRING) +
                     " websocket-client-coro");
       }));
-  ws.next_layer().async_handshake(boost::asio::ssl::stream_base::client,
-                                  yield[ec]);
+  m_ws.next_layer().async_handshake(boost::asio::ssl::stream_base::client,
+                                    yield[ec]);
   if (ec)
     return fail(ec, "ssl_handshake");
-  boost::beast::get_lowest_layer(ws).expires_never();
-  ws.set_option(boost::beast::websocket::stream_base::timeout::suggested(
+  boost::beast::get_lowest_layer(m_ws).expires_never();
+  m_ws.set_option(boost::beast::websocket::stream_base::timeout::suggested(
       boost::beast::role_type::client));
-  ws.async_handshake(host, "/", yield[ec]);
+  m_ws.async_handshake(host, "/", yield[ec]);
   if (ec)
     return fail(ec, "handshake");
+}
+
+void session_t::run_heartbeat(yield_context_t yield) {
+
+  boost::beast::error_code ec;
 
   // TODO: https://stackoverflow.com/a/30560228
   // use timer scheme described for ping/pong loop
@@ -83,19 +81,19 @@ void session_t::run_heartbeat(std::string host, std::string port,
     ping["event"] = "ping";
     ping["reqid"] = ++id;
 
-    ws.async_write(boost::asio::buffer(ping.dump()), yield[ec]);
+    m_ws.async_write(boost::asio::buffer(ping.dump()), yield[ec]);
     if (ec)
       return fail(ec, "write");
 
     buffer.clear();
-    ws.async_read(buffer, yield[ec]);
+    m_ws.async_read(buffer, yield[ec]);
 
     if (ec)
       return fail(ec, "read");
     BOOST_LOG_TRIVIAL(info) << boost::beast::make_printable(buffer.data());
   }
 
-  ws.async_close(boost::beast::websocket::close_code::normal, yield[ec]);
+  m_ws.async_close(boost::beast::websocket::close_code::normal, yield[ec]);
   if (ec)
     return fail(ec, "close");
 }
--- session.hpp
+++ session.hpp
@@ -1,6 +1,13 @@
+#pragma once
+
+#define BOOST_ASIO_DISABLE_BOOST_COROUTINE
+
 #include <boost/asio.hpp>
 #include <boost/asio/spawn.hpp>
+#include <boost/beast/core.hpp>
 #include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
 
 #include <string>
 
@@ -10,17 +17,19 @@ struct session_t final {
 
   using ioc_t = boost::asio::io_context;
   using ssl_context_t = boost::asio::ssl::context;
+  using websocket_t = boost::beast::websocket::stream<
+      boost::beast::ssl_stream<boost::beast::tcp_stream>>;
   using yield_context_t = boost::asio::yield_context;
 
   session_t(ioc_t &, ssl_context_t &);
 
-  void start_heartbeat(std::string host, std::string port);
-
 private:
-  void run_heartbeat(std::string host, std::string port, yield_context_t);
+  void handshake(std::string host, std::string port, yield_context_t);
+  void run_heartbeat(yield_context_t);
 
   ioc_t &m_ioc;
   ssl_context_t &m_ssl_context;
+  websocket_t m_ws;
 };
 
 } // namespace krakpot
