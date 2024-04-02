96e755a3d4544fe4d6a8003770f933cc2355f6bd
John Finley <torquey@gmail.com>
2024-04-02 13:33:46 -0500

    Tweak session and processor

--- session.cpp
+++ session.cpp
@@ -4,10 +4,13 @@
 
 #include <chrono>
 #include <format>
-#include <sstream>
+
+namespace asio = boost::asio;
+namespace bst = boost::beast;
+namespace ws = bst::websocket;
 
 namespace {
-void fail(boost::beast::error_code ec, char const *what) {
+void fail(bst::error_code ec, char const *what) {
   BOOST_LOG_TRIVIAL(error) << what << ": " << ec.message();
   throw std::runtime_error(what);
 }
@@ -18,10 +21,11 @@ namespace krakpot {
 session_t::session_t(ioc_t &ioc, ssl_context_t &ssl_context)
     : m_ioc{ioc}, m_ssl_context{ssl_context}, m_ws{ioc, ssl_context} {
 
-  boost::asio::spawn(
+  asio::spawn(
       m_ioc,
       [this](yield_context_t yc) {
         handshake("ws.kraken.com", "443", yc);
+        subscribe(yc);
         ping(yc);
       },
       [](std::exception_ptr ex) {
@@ -33,54 +37,54 @@ session_t::session_t(ioc_t &ioc, ssl_context_t &ssl_context)
 void session_t::handshake(std::string host, std::string port,
                           yield_context_t yield) {
 
-  BOOST_LOG_TRIVIAL(debug) << "handshake() entered";
+  bst::error_code ec;
 
-  boost::beast::error_code ec;
-
-  boost::asio::ip::tcp::resolver resolver(m_ioc);
+  asio::ip::tcp::resolver resolver(m_ioc);
   auto const results = resolver.async_resolve(host, port, yield[ec]);
-  if (ec)
+  if (ec) {
     return fail(ec, "resolve");
-  boost::beast::get_lowest_layer(m_ws).expires_after(std::chrono::seconds(30));
-  auto ep =
-      boost::beast::get_lowest_layer(m_ws).async_connect(results, yield[ec]);
-  if (ec)
-    return fail(ec, "connect");
-  if (!SSL_set_tlsext_host_name(m_ws.next_layer().native_handle(),
-                                host.c_str())) {
-    ec = boost::beast::error_code(static_cast<int>(::ERR_get_error()),
-                                  boost::asio::error::get_ssl_category());
+  }
+
+  bst::get_lowest_layer(m_ws).expires_after(std::chrono::seconds(30));
+  auto endpoint = bst::get_lowest_layer(m_ws).async_connect(results, yield[ec]);
+  if (ec) {
     return fail(ec, "connect");
   }
-  host += ':' + std::to_string(ep.port());
-  boost::beast::get_lowest_layer(m_ws).expires_after(std::chrono::seconds(30));
-  m_ws.set_option(boost::beast::websocket::stream_base::decorator(
-      [](boost::beast::websocket::request_type &req) {
-        req.set(boost::beast::http::field::user_agent,
-                std::string(BOOST_BEAST_VERSION_STRING) +
-                    " websocket-client-coro");
-      }));
-  m_ws.next_layer().async_handshake(boost::asio::ssl::stream_base::client,
-                                    yield[ec]);
-  if (ec)
+
+  if (!SSL_set_tlsext_host_name(m_ws.next_layer().native_handle(),
+                                host.c_str())) {
+    ec = bst::error_code(static_cast<int>(::ERR_get_error()),
+                         asio::error::get_ssl_category());
+    return fail(ec, "connect");
+  }
+
+  host += ':' + std::to_string(endpoint.port());
+  bst::get_lowest_layer(m_ws).expires_after(std::chrono::seconds(30));
+  m_ws.set_option(ws::stream_base::decorator([](ws::request_type &req) {
+    req.set(bst::http::field::user_agent,
+            std::string(BOOST_BEAST_VERSION_STRING) + " websocket-client-coro");
+  }));
+  m_ws.next_layer().async_handshake(asio::ssl::stream_base::client, yield[ec]);
+  if (ec) {
     return fail(ec, "ssl_handshake");
-  boost::beast::get_lowest_layer(m_ws).expires_never();
-  m_ws.set_option(boost::beast::websocket::stream_base::timeout::suggested(
-      boost::beast::role_type::client));
+  }
+
+  bst::get_lowest_layer(m_ws).expires_never();
+  m_ws.set_option(ws::stream_base::timeout::suggested(bst::role_type::client));
   m_ws.async_handshake(host, "/v2", yield[ec]);
-  if (ec)
+  if (ec) {
     return fail(ec, "handshake");
+  }
 }
 
 void session_t::ping(yield_context_t yield) {
 
-  boost::beast::error_code ec;
-  boost::beast::flat_buffer buffer;
-  boost::asio::deadline_timer timer(m_ioc);
-  subscribe(yield);
+  bst::error_code ec;
+  bst::flat_buffer buffer;
+  asio::deadline_timer timer(m_ioc);
   while (true) {
     const auto ping = request::ping_t{++m_req_id};
-    m_ws.async_write(boost::asio::buffer(ping.str()), yield[ec]);
+    m_ws.async_write(asio::buffer(ping.str()), yield[ec]);
     if (ec)
       return fail(ec, "write");
 
@@ -88,20 +92,20 @@ void session_t::ping(yield_context_t yield) {
     timer.async_wait(yield);
   }
 
-  m_ws.async_close(boost::beast::websocket::close_code::normal, yield[ec]);
+  m_ws.async_close(ws::close_code::normal, yield[ec]);
   if (ec)
     return fail(ec, "close");
 }
 
 void session_t::subscribe(yield_context_t yield) {
 
-  boost::beast::error_code ec;
-  boost::beast::flat_buffer buffer;
+  bst::error_code ec;
+  bst::flat_buffer buffer;
 
   const request::subscribe_instrument_t subscribe_inst{++m_req_id};
   BOOST_LOG_TRIVIAL(debug) << "send: " << subscribe_inst.str();
 
-  m_ws.async_write(boost::asio::buffer(subscribe_inst.str()), yield[ec]);
+  m_ws.async_write(asio::buffer(subscribe_inst.str()), yield[ec]);
   if (ec)
     return fail(ec, "write");
 
@@ -115,13 +119,13 @@ void session_t::subscribe(yield_context_t yield) {
       ++m_req_id, request::subscribe_book_t::e_100, true, symbols};
   BOOST_LOG_TRIVIAL(debug) << "send: " << subscribe_book.str();
 
-  m_ws.async_write(boost::asio::buffer(subscribe_book.str()), yield[ec]);
+  m_ws.async_write(asio::buffer(subscribe_book.str()), yield[ec]);
   if (ec)
     return fail(ec, "write");
 }
 
 void session_t::start_processing() {
-  boost::asio::spawn(
+  asio::spawn(
       m_ioc, [this](yield_context_t yc) { process(yc); },
       [](std::exception_ptr ex) {
         if (ex)
@@ -131,9 +135,9 @@ void session_t::start_processing() {
 
 void session_t::process(yield_context_t yield) {
 
-  boost::beast::error_code ec;
-  boost::beast::flat_buffer buffer;
-  boost::asio::deadline_timer timer(m_ioc);
+  bst::error_code ec;
+  bst::flat_buffer buffer;
+  asio::deadline_timer timer(m_ioc);
   timer.expires_from_now(boost::posix_time::seconds(3));
   timer.async_wait(yield);
 
@@ -143,7 +147,7 @@ void session_t::process(yield_context_t yield) {
     if (ec) {
       fail(ec, "read");
     } else {
-      auto json = boost::beast::buffers_to_string(buffer.data());
+      auto json = bst::buffers_to_string(buffer.data());
       try {
         m_processor.process(json);
       } catch (const std::exception &ex) {
