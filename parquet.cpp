7cd7d3ddb56d34235950c301ad3417b96be639d8
John Finley <torquey@gmail.com>
2024-04-15 15:58:05 -0500

    Improve timestamp_t and fix response round-tripping

--- parquet.cpp
+++ parquet.cpp
@@ -32,7 +32,7 @@ void trades_sink_t::accept(const response::trades_t &trades) {
     PARQUET_THROW_NOT_OK(m_qty_builder.Append(trade.qty));
     PARQUET_THROW_NOT_OK(m_side_builder.Append(std::string(1, trade.side)));
     PARQUET_THROW_NOT_OK(m_symbol_builder.Append(trade.symbol));
-    PARQUET_THROW_NOT_OK(m_timestamp_builder.Append(trade.timestamp));
+    PARQUET_THROW_NOT_OK(m_timestamp_builder.Append(trade.timestamp.micros()));
     PARQUET_THROW_NOT_OK(m_trade_id_builder.Append(trade.trade_id));
   }
 
