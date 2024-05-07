cdff49a007f7e60e38684cfc3143bdee91c806aa
John Finley <torquey@gmail.com>
2024-05-07 11:13:30 -0500

    Trim whitespace from decimal strings

--- decimal.cpp
+++ decimal.cpp
@@ -8,7 +8,10 @@ std::string token_t::trimmed() const {
   // !@# TODO upgrade compiler and use ranges
   // !@# TODO verify that this is not a bottleneck
   auto buffer = m_chars;
-  const auto eit = std::remove(buffer.begin(), buffer.end(), '.');
+  auto eit = std::remove(buffer.begin(), buffer.end(), '.');
+  buffer.erase(eit, buffer.end());
+  eit = std::remove_if(buffer.begin(), buffer.end(),
+                       [](const char ch) { return std::iswspace(ch); });
   buffer.erase(eit, buffer.end());
   const auto begin = buffer.find_first_not_of('0');
   buffer = buffer.substr(begin);
