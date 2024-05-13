053cf05d589ecc86ed6279ca162592498fd6fe6a
John Finley <torquey@gmail.com>
2024-05-13 15:06:01 -0500

    Fix crc32 calculation to ignore non-digits

--- decimal.cpp
+++ decimal.cpp
@@ -7,7 +7,7 @@ namespace krakpot {
 void token_t::process(boost::crc_32_type &crc32) const {
   auto in_leading_zeros = true;
   for (const auto ch : m_chars) {
-    if (ch != '.') {
+    if (std::isdigit(ch)) {
       if (!in_leading_zeros) {
         crc32.process_byte(ch);
       } else if (ch != '0') {
