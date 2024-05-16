0e1363190e795ae38a01359c110fb5b2411897fc
John Finley <torquey@gmail.com>
2024-05-16 14:00:26 -0500

    Convert more strings to constants

--- requests.cpp
+++ requests.cpp
@@ -8,38 +8,40 @@ namespace krakpot {
 namespace request {
 
 nlohmann::json ping_t::to_json() const {
-  const nlohmann::json result = {{"method", "ping"}, {"req_id", m_req_id}};
+  const nlohmann::json result = {{c_request_method, c_method_ping},
+                                 {c_request_req_id, m_req_id}};
   return result;
 }
 
 nlohmann::json subscribe_instrument_t::to_json() const {
-  const nlohmann::json result = {
-      {"method", "subscribe"},
-      {"params", {{"channel", "instrument"}, {"snapshot", m_snapshot}}},
-      {"req_id", m_req_id}};
+  const nlohmann::json result = {{c_request_method, c_method_subscribe},
+                                 {c_request_params,
+                                  {{c_request_channel, c_channel_instrument},
+                                   {c_param_snapshot, m_snapshot}}},
+                                 {c_request_req_id, m_req_id}};
   return result;
 }
 
 nlohmann::json subscribe_book_t::to_json() const {
-  const nlohmann::json result = {{"method", "subscribe"},
-                                 {"params",
-                                  {{"channel", "book"},
-                                   {"depth", m_depth},
-                                   {"snapshot", m_snapshot},
-                                   {"symbol", m_symbols}}},
-                                 {"req_id", m_req_id}};
+  const nlohmann::json result = {{c_request_method, c_method_subscribe},
+                                 {c_request_params,
+                                  {{c_request_channel, c_channel_book},
+                                   {c_param_depth, m_depth},
+                                   {c_param_snapshot, m_snapshot},
+                                   {c_param_symbol, m_symbols}}},
+                                 {c_request_req_id, m_req_id}};
   return result;
 }
 
 nlohmann::json subscribe_trade_t::to_json() const {
-  const nlohmann::json result = {{"method", "subscribe"},
-                                 {"params",
+  const nlohmann::json result = {{c_request_method, c_method_subscribe},
+                                 {c_request_params,
                                   {
-                                      {"channel", "trade"},
-                                      {"symbol", m_symbols},
-                                      {"snapshot", m_snapshot},
+                                      {c_request_channel, c_channel_trade},
+                                      {c_param_snapshot, m_snapshot},
+                                      {c_param_symbol, m_symbols},
                                   }},
-                                 {"req_id", m_req_id}};
+                                 {c_request_req_id, m_req_id}};
   return result;
 }
 
