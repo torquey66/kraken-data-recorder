dd4dc4955627ff7f194188566f078b24d5578f82
John Finley <torquey@gmail.com>
2024-05-16 14:00:26 -0500

    Convert more strings to constants

--- constants.hpp
+++ constants.hpp
@@ -29,8 +29,16 @@ static constexpr char c_header_recv_tm[] = "recv_tm";
 static constexpr char c_header_channel[] = "channel";
 static constexpr char c_header_type[] = "type";
 
-static constexpr char c_response_data[] = "data";
+static constexpr char c_request_channel[] = "channel";
+static constexpr char c_request_method[] = "method";
+static constexpr char c_request_params[] = "params";
+static constexpr char c_request_req_id[] = "req_id";
 
+static constexpr char c_param_depth[] = "depth";
+static constexpr char c_param_snapshot[] = "snapshot";
+static constexpr char c_param_symbol[] = "symbol";
+
+static constexpr char c_response_data[] = "data";
 static constexpr char c_response_channel[] = "channel";
 static constexpr char c_response_method[] = "method";
 
@@ -39,7 +47,9 @@ static constexpr char c_channel_book[] = "book";
 static constexpr char c_channel_trade[] = "trade";
 static constexpr char c_channel_heartbeat[] = "heartbeat";
 
+static constexpr char c_method_ping[] = "ping";
 static constexpr char c_method_pong[] = "pong";
+static constexpr char c_method_subscribe[] = "subscribe";
 
 static constexpr char c_instrument_assets[] = "assets";
 static constexpr char c_instrument_pairs[] = "pairs";
