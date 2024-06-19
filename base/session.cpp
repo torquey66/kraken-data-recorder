73fd13d287368d5ac6a1c7866cd9f7e20a3957a2
John Finley <torquey@gmail.com>
2024-06-19 16:41:43 -0500

    Quick patch to evaluate fix

--- base/session.cpp
+++ base/session.cpp
@@ -59,7 +59,7 @@ void session_t::start_processing(const recv_cb_t &handle_recv) {
 
   m_read_buffer.clear();
   m_ws.async_read(m_read_buffer,
-                  [this, &handle_recv](error_code ec, size_t size) {
+                  [this, handle_recv](error_code ec, size_t size) {
                     this->on_read(ec, size, handle_recv);
                   });
 }
@@ -216,7 +216,7 @@ void session_t::on_read(error_code ec, size_t size,
   if (m_keep_processing) {
     m_read_msg_str.clear();
     m_ws.async_read(m_read_buffer,
-                    [this, &handle_recv](error_code ec, size_t size) {
+                    [this, handle_recv](error_code ec, size_t size) {
                       this->on_read(ec, size, handle_recv);
                     });
   }
