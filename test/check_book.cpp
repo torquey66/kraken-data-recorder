e3ad88171b0dfbbfb4478cb7405c4a7df1f4b452
John Finley <torquey@gmail.com>
2024-07-02 07:56:19 -0500

    WIP on improving tests

--- test/check_book.cpp
+++ test/check_book.cpp
@@ -7,8 +7,8 @@
 #include <arrow/io/api.h>
 #include <arrow/record_batch.h>
 #include <arrow/scalar.h>
-#include <nlohmann/json.hpp>
 #include <parquet/arrow/reader.h>
+#include <nlohmann/json.hpp>
 
 #include <iomanip>
 #include <iostream>
@@ -16,9 +16,7 @@
 
 using namespace krakpot;
 
-// std::vector<quote_t> extract(const arrow::ListArray &quote_col,
-// arrow::int64_t idx) {
-std::vector<quote_t> extract(const arrow::ListArray &quotes_array,
+std::vector<quote_t> extract(const arrow::ListArray& quotes_array,
                              int64_t idx) {
   auto result = std::vector<quote_t>{};
   const auto slice = std::dynamic_pointer_cast<arrow::StructArray>(
@@ -41,9 +39,9 @@ std::vector<quote_t> extract(const arrow::ListArray &quotes_array,
   return result;
 }
 
-void dump_sides(const model::sides_t &sides, const size_t max_depth = 10) {
-  const auto &bids = sides.bids();
-  const auto &asks = sides.asks();
+void dump_sides(const model::sides_t& sides, const size_t max_depth = 10) {
+  const auto& bids = sides.bids();
+  const auto& asks = sides.asks();
   auto bid_it = bids.begin();
   auto ask_it = asks.begin();
   for (size_t depth = 0; depth < max_depth; ++depth) {
@@ -58,15 +56,134 @@ void dump_sides(const model::sides_t &sides, const size_t max_depth = 10) {
   }
 }
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "usage: " << argv[0] << " <book parquet file>" << std::endl;
+void process_pairs(std::string pairs_filename,
+                   model::level_book_t& level_book) {
+  auto* pool = arrow::default_memory_pool();
+  auto reader_properties = parquet::ReaderProperties(pool);
+  reader_properties.set_buffer_size(4096 * 4);
+  reader_properties.enable_buffered_stream();
+
+  auto arrow_reader_props = parquet::ArrowReaderProperties();
+  arrow_reader_props.set_batch_size(128 * 1024);
+
+  parquet::arrow::FileReaderBuilder reader_builder;
+  PARQUET_THROW_NOT_OK(
+      reader_builder.OpenFile(pairs_filename, false, reader_properties));
+  reader_builder.memory_pool(pool);
+  reader_builder.properties(arrow_reader_props);
+
+  std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
+  PARQUET_ASSIGN_OR_THROW(arrow_reader, reader_builder.Build());
+
+  std::shared_ptr<::arrow::RecordBatchReader> rb_reader;
+  PARQUET_THROW_NOT_OK(arrow_reader->GetRecordBatchReader(&rb_reader));
+
+  for (arrow::Result<std::shared_ptr<arrow::RecordBatch>> maybe_batch :
+       *rb_reader) {
+    if (!maybe_batch.ok()) {
+      std::cerr << "error: " << maybe_batch.status().ToString() << std::endl;
+    }
+
+    const auto& batch = *maybe_batch.ValueOrDie();
+    // const auto recv_tm_array = std::dynamic_pointer_cast<arrow::Int64Array>(
+    //     batch.GetColumnByName(c_header_recv_tm));
+    const auto base_array = std::dynamic_pointer_cast<arrow::StringArray>(
+        batch.GetColumnByName(c_pair_base));
+    const auto cost_min_array = std::dynamic_pointer_cast<arrow::DoubleArray>(
+        batch.GetColumnByName(c_pair_cost_min));
+    const auto cost_precision_array =
+        std::dynamic_pointer_cast<arrow::Int64Array>(
+            batch.GetColumnByName(c_pair_cost_precision));
+    const auto has_index_array = std::dynamic_pointer_cast<arrow::BooleanArray>(
+        batch.GetColumnByName(c_pair_has_index));
+    const auto margin_initial_array =
+        std::dynamic_pointer_cast<arrow::DoubleArray>(
+            batch.GetColumnByName(c_pair_margin_initial));
+    const auto marginable_array =
+        std::dynamic_pointer_cast<arrow::BooleanArray>(
+            batch.GetColumnByName(c_pair_marginable));
+    const auto position_limit_long_array =
+        std::dynamic_pointer_cast<arrow::Int64Array>(
+            batch.GetColumnByName(c_pair_position_limit_long));
+    const auto position_limit_short_array =
+        std::dynamic_pointer_cast<arrow::Int64Array>(
+            batch.GetColumnByName(c_pair_position_limit_short));
+    const auto price_increment_array =
+        std::dynamic_pointer_cast<arrow::DoubleArray>(
+            batch.GetColumnByName(c_pair_price_increment));
+    const auto price_precision_array =
+        std::dynamic_pointer_cast<arrow::Int64Array>(
+            batch.GetColumnByName(c_pair_price_precision));
+    const auto qty_increment_array =
+        std::dynamic_pointer_cast<arrow::DoubleArray>(
+            batch.GetColumnByName(c_pair_qty_increment));
+    const auto qty_min_array = std::dynamic_pointer_cast<arrow::DoubleArray>(
+        batch.GetColumnByName(c_pair_qty_min));
+    const auto qty_precision_array =
+        std::dynamic_pointer_cast<arrow::Int64Array>(
+            batch.GetColumnByName(c_pair_qty_precision));
+    const auto quote_array = std::dynamic_pointer_cast<arrow::StringArray>(
+        batch.GetColumnByName(c_pair_quote));
+    const auto status_array = std::dynamic_pointer_cast<arrow::Int8Array>(
+        batch.GetColumnByName(c_pair_status));
+    const auto symbol_array = std::dynamic_pointer_cast<arrow::StringArray>(
+        batch.GetColumnByName(c_pair_symbol));
+
+    for (auto idx = 0; idx < batch.num_rows(); ++idx) {
+      //      const auto recv_tm = recv_tm_array->Value(idx);
+      const auto base = base_array->Value(idx);
+      const auto cost_min = cost_min_array->Value(idx);
+      const auto cost_precision = cost_precision_array->Value(idx);
+      const auto has_index = has_index_array->Value(idx);
+      const auto margin_initial = margin_initial_array->Value(idx);
+      const auto marginable = marginable_array->Value(idx);
+      const auto position_limit_long = position_limit_long_array->Value(idx);
+      const auto position_limit_short = position_limit_short_array->Value(idx);
+      const auto price_increment = price_increment_array->Value(idx);
+      const auto price_precision = price_precision_array->Value(idx);
+      const auto qty_increment = qty_increment_array->Value(idx);
+      const auto qty_min = qty_min_array->Value(idx);
+      const auto qty_precision = qty_precision_array->Value(idx);
+      const auto quote = quote_array->Value(idx);
+      const auto status = status_array->Value(idx);
+      const auto symbol = symbol_array->Value(idx);
+
+      const auto pair =
+          response::pair_t{std::string{base.begin(), base.end()},
+                           cost_min,
+                           cost_precision,
+                           has_index,
+                           margin_initial,
+                           marginable,
+                           position_limit_long,
+                           position_limit_short,
+                           price_increment,
+                           price_precision,
+                           qty_increment,
+                           qty_min,
+                           qty_precision,
+                           std::string{quote.begin(), quote.end()},
+                           response::pair_t::status_t{status},
+                           std::string{symbol.begin(), symbol.end()}};
+      level_book.accept(pair);
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cerr << "usage: " << argv[0] << " <pairs parquet file>"
+              << " <book parquet file>" << std::endl;
     return -1;
   }
 
-  const auto filename = std::string{argv[1]};
+  const auto pairs_filename = std::string{argv[1]};
+  const auto filename = std::string{argv[2]};
 
-  auto *pool = arrow::default_memory_pool();
+  auto level_book = model::level_book_t{e_1000};
+  process_pairs(pairs_filename, level_book);
+
+  auto* pool = arrow::default_memory_pool();
   auto reader_properties = parquet::ReaderProperties(pool);
   reader_properties.set_buffer_size(4096 * 4);
   reader_properties.enable_buffered_stream();
@@ -86,14 +203,12 @@ int main(int argc, char *argv[]) {
   std::shared_ptr<::arrow::RecordBatchReader> rb_reader;
   PARQUET_THROW_NOT_OK(arrow_reader->GetRecordBatchReader(&rb_reader));
 
-  auto level_book = model::level_book_t{e_1000};
-
   for (arrow::Result<std::shared_ptr<arrow::RecordBatch>> maybe_batch :
        *rb_reader) {
     if (!maybe_batch.ok()) {
       std::cerr << "error: " << maybe_batch.status().ToString() << std::endl;
     }
-    const auto &batch = *maybe_batch.ValueOrDie();
+    const auto& batch = *maybe_batch.ValueOrDie();
     const auto recv_tm_array = std::dynamic_pointer_cast<arrow::Int64Array>(
         batch.GetColumnByName(c_header_recv_tm));
     const auto type_array = std::dynamic_pointer_cast<arrow::StringArray>(
@@ -124,12 +239,55 @@ int main(int argc, char *argv[]) {
           response::book_t{header, asks, bids, crc32, symbol_str, timestamp};
       try {
         level_book.accept(response);
-      } catch (const std::exception &ex) {
+      } catch (const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
         std::cerr << response.str() << std::endl;
         dump_sides(level_book.sides(symbol_str));
         return -1;
       }
     }
+  }
+
+  /*
+     handle_msg: bogus crc32 expected: 2542650875 actual: 1140764732 msg:
+     {"channel":"book","type":"update","data":[{"symbol":"SOL/USD","bids":[{"price":146.91,"qty":917.15439679}],"asks":[],"checksum":2542650875,"timestamp":"2024-07-01T19:30:11.454069Z"}]}
+  */
+
+  const std::string bad_msg = R"MSG(
+  {
+    "channel": "book",
+    "type": "update",
+    "data": [
+      {
+        "symbol": "SOL/USD",
+        "bids": [
+           {"price":146.91,"qty":917.15439679}
+        ],
+        "asks": [],
+        "checksum": 2542650875,
+        "timestamp":"2024-07-01T19:30:11.454069Z"
+      }
+    ]
+  }
+  )MSG";
+  simdjson::ondemand::parser parser;
+  simdjson::padded_string bad_response{bad_msg};
+  simdjson::ondemand::document bad_doc = parser.iterate(bad_response);
+  const auto bad_update = krakpot::response::book_t::from_json(bad_doc);
+  try {
+    dump_sides(level_book.sides(bad_update.symbol()));
+    std::cout << "checksum: " << level_book.crc32(bad_update.symbol())
+              << std::endl;
+    std::cout << "-------------------------------------------------------------"
+                 "-------------------"
+              << std::endl;
+    level_book.accept(bad_update);
+  } catch (const std::exception& ex) {
+    std::cerr << ex.what() << std::endl;
+    std::cerr << bad_update.str() << std::endl;
+    dump_sides(level_book.sides(bad_update.symbol()));
+    std::cout << "checksum: " << level_book.crc32(bad_update.symbol())
+              << std::endl;
+    return -1;
   }
 }
--- test/unit/level_book_test.cpp
+++ test/unit/level_book_test.cpp
@@ -135,7 +135,8 @@ static const std::string example_update_str = R"RESPONSE(
   45285210000045286415457195345286615457110945289615456091145290215890660452918154553491452947445474945296135380000452975994554245299518772827
 */
 
-TEST_CASE("book_t doc example snapshot") {
+/*
+  TEST_CASE("book_t doc example snapshot") {
   simdjson::ondemand::parser parser;
   simdjson::padded_string snap_response{example_snapshot_str};
   simdjson::ondemand::document snap_doc = parser.iterate(snap_response);
@@ -145,10 +146,11 @@ TEST_CASE("book_t doc example snapshot") {
   auto book = krakpot::model::level_book_t{krakpot::e_100};
   book.accept(snap);
   CHECK(book.crc32("BTC/USD") == snap.crc32());
-}
+  }
+*/
 
 /*
-TEST_CASE("book_t doc example update") {
+  TEST_CASE("book_t doc example update") {
   simdjson::ondemand::parser parser;
   simdjson::padded_string snap_response{example_snapshot_str};
   simdjson::ondemand::document snap_doc = parser.iterate(snap_response);
@@ -158,5 +160,141 @@ TEST_CASE("book_t doc example update") {
   simdjson::ondemand::document update_doc = parser.iterate(update_response);
 
   CHECK(book.crc32() == 2439117997);
-}
+  }
 */
+
+static const std::string pair_str = R"RESPONSE(
+[
+  {
+    "recv_tm": 1719859326224708,
+    "base": "GST",
+    "cost_min": 0.5,
+    "cost_precision": 5,
+    "has_index": false,
+    "marginable": false,
+    "price_increment": 0.001,
+    "price_precision": 3,
+    "qty_increment": 1E-8,
+    "qty_min": 200.0,
+    "qty_precision": 8,
+    "quote": "USD",
+    "status": "online",
+    "symbol": "GST/USD"
+  }
+]
+)RESPONSE";
+
+static const std::string snapshot_str = R"RESPONSE(
+{
+  "channel": "book",
+  "type": "snapshot",
+  "data": [
+    {
+      "symbol": "GST/USD",
+      "bids": [
+        {
+          "price": 0.016,
+          "qty": 255965.95133811
+        },
+        {
+          "price": 0.015,
+          "qty": 264465.46682136
+        },
+        {
+          "price": 0.014,
+          "qty": 198234.50375152
+        },
+        {
+          "price": 0.013,
+          "qty": 263077.71115063
+        },
+        {
+          "price": 0.012,
+          "qty": 135283.23181445
+        },
+        {
+          "price": 0.011,
+          "qty": 232726.34707055
+        },
+        {
+          "price": 0.010,
+          "qty": 211909.56878553
+        },
+        {
+          "price": 0.009,
+          "qty": 16666.66666666
+        },
+        {
+          "price": 0.008,
+          "qty": 13600.00000000
+        },
+        {
+          "price": 0.007,
+          "qty": 1000.00000000
+        }
+      ],
+      "asks": [
+        {
+          "price": 0.017,
+          "qty": 94510.50669693
+        },
+        {
+          "price": 0.018,
+          "qty": 232489.98702916
+        },
+        {
+          "price": 0.019,
+          "qty": 244770.01655926
+        },
+        {
+          "price": 0.020,
+          "qty": 103394.23779803
+        },
+        {
+          "price": 0.021,
+          "qty": 120226.44704447
+        },
+        {
+          "price": 0.022,
+          "qty": 122811.44535027
+        },
+        {
+          "price": 0.023,
+          "qty": 185766.68965043
+        },
+        {
+          "price": 0.024,
+          "qty": 95339.83830809
+        },
+        {
+          "price": 0.025,
+          "qty": 32960.86333331
+        },
+        {
+          "price": 0.026,
+          "qty": 86326.77204454
+        }
+      ],
+      "checksum": 1931231958
+    }
+  ]
+}
+)RESPONSE";
+
+TEST_CASE("book_t doc example snapshot") {
+  auto book = krakpot::model::level_book_t{krakpot::e_10};
+
+  simdjson::ondemand::parser parser;
+
+  simdjson::padded_string pair_response{pair_str};
+  simdjson::ondemand::document pair_doc = parser.iterate(pair_response);
+  for (simdjson::fallback::ondemand::object pair_obj : pair_doc) {
+    const auto pair = krakpot::response::pair_t::from_json(pair_obj);
+    book.accept(pair);
+  }
+
+  simdjson::padded_string snap_response{snapshot_str};
+  simdjson::ondemand::document snap_doc = parser.iterate(snap_response);
+  const auto snap = krakpot::response::book_t::from_json(snap_doc);
+  book.accept(snap);
+}
