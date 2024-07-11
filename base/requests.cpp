e2e4c13ecbe7b74ec33e4fa6b9be9c8fd26cefd4
John Finley <torquey@gmail.com>
2024-07-11 11:32:57 -0500

    Convert more json serializables to boost::json

--- base/requests.cpp
+++ base/requests.cpp
@@ -2,48 +2,55 @@
 
 #include "requests.hpp"
 
-#include <nlohmann/json.hpp>
+#include <algorithm>
 
 namespace krakpot {
 namespace request {
 
-nlohmann::json ping_t::to_json() const {
-  const nlohmann::json result = {{c_request_method, c_method_ping},
-                                 {c_request_req_id, m_req_id}};
+boost::json::object ping_t::to_json_obj() const {
+  const boost::json::object result = {{c_request_method, c_method_ping},
+                                      {c_request_req_id, m_req_id}};
   return result;
 }
 
-nlohmann::json subscribe_instrument_t::to_json() const {
-  const nlohmann::json result = {{c_request_method, c_method_subscribe},
-                                 {c_request_params,
-                                  {{c_request_channel, c_channel_instrument},
-                                   {c_param_snapshot, m_snapshot}}},
-                                 {c_request_req_id, m_req_id}};
+boost::json::object subscribe_instrument_t::to_json_obj() const {
+  const boost::json::object result = {
+      {c_request_method, c_method_subscribe},
+      {c_request_params,
+       {{c_request_channel, c_channel_instrument},
+        {c_param_snapshot, m_snapshot}}},
+      {c_request_req_id, m_req_id}};
   return result;
 }
 
-nlohmann::json subscribe_book_t::to_json() const {
-  const nlohmann::json result = {{c_request_method, c_method_subscribe},
-                                 {c_request_params,
-                                  {{c_request_channel, c_channel_book},
-                                   {c_param_depth, m_depth},
-                                   {c_param_snapshot, m_snapshot},
-                                   {c_param_symbol, m_symbols}}},
-                                 {c_request_req_id, m_req_id}};
+boost::json::object subscribe_book_t::to_json_obj() const {
+  auto symbol_objs = boost::json::array{};
+  std::transform(
+      m_symbols.begin(), m_symbols.end(), std::back_inserter(symbol_objs),
+      [](const std::string& symbol) { return boost::json::string{symbol}; });
+  const boost::json::object result = {{c_request_method, c_method_subscribe},
+                                      {c_request_params,
+                                       {{c_request_channel, c_channel_book},
+                                        {c_param_depth, m_depth},
+                                        {c_param_snapshot, m_snapshot},
+                                        {c_param_symbol, symbol_objs}}},
+                                      {c_request_req_id, m_req_id}};
   return result;
 }
 
-nlohmann::json subscribe_trade_t::to_json() const {
-  const nlohmann::json result = {{c_request_method, c_method_subscribe},
-                                 {c_request_params,
-                                  {
-                                      {c_request_channel, c_channel_trade},
-                                      {c_param_snapshot, m_snapshot},
-                                      {c_param_symbol, m_symbols},
-                                  }},
-                                 {c_request_req_id, m_req_id}};
+boost::json::object subscribe_trade_t::to_json_obj() const {
+  auto symbol_objs = boost::json::array{};
+  std::transform(
+      m_symbols.begin(), m_symbols.end(), std::back_inserter(symbol_objs),
+      [](const std::string& symbol) { return boost::json::string{symbol}; });
+  const boost::json::object result = {{c_request_method, c_method_subscribe},
+                                      {c_request_params,
+                                       {{c_request_channel, c_channel_trade},
+                                        {c_param_snapshot, m_snapshot},
+                                        {c_param_symbol, symbol_objs}}},
+                                      {c_request_req_id, m_req_id}};
   return result;
 }
 
-} // namespace request
-} // namespace krakpot
+}  // namespace request
+}  // namespace krakpot
