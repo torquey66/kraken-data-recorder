d937796a3502d0f0e790bbbb24996dcbbb6e1cc8
John Finley <torquey@gmail.com>
2024-08-07 11:18:50 -0500

    Generate code for side enum

--- base/types.hpp
+++ base/types.hpp
@@ -33,32 +33,6 @@ using ask_t = quote_t;
 using bid_t = quote_t;
 
 /**
- * side_t is based on the FIX 5.0 spec. See:
- *
- * https://www.onixs.biz/fix-dictionary/5.0/tagNum_54.html
- */
-enum side_t : char {
-  // clang-format off
-  e_buy                = '1',
-  e_sell               = '2',
-  e_buy_minus          = '3',
-  e_sell_plus          = '4',
-  e_sell_short         = '5',
-  e_sell_short_exempt  = '6',
-  e_undisclosed        = '7',
-  e_cross              = '8',
-  e_cross_short        = '9',
-  e_cross_short_exempt = 'A',
-  e_as_defined         = 'B',
-  e_opposite           = 'C',
-  e_subscribe          = 'D',
-  e_redeem             = 'E',
-  e_lend               = 'F',
-  e_borrow             = 'G',
-  // clang-format on
-};
-
-/**
  * AFAIK, Kraken timetamps are always ISO 8601 strings in GMT.
  *
  * TODO: consider using chrono timepoints in some fashion instead of
@@ -73,11 +47,11 @@ struct timestamp_t final {
   int64_t micros() const { return m_micros; }
 
   static std::string to_iso_8601(int64_t);
-  static int64_t from_iso_8601(const std::string &);
+  static int64_t from_iso_8601(const std::string&);
 
   static timestamp_t now();
 
-private:
+ private:
   int64_t m_micros = 0;
 };
 
