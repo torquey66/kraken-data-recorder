29e7f261af68b376cd9f0e306c78f368f90126f9
John Finley <torquey@gmail.com>
2024-08-30 09:45:26 -0500

    Fix decimal_test failures

--- include/decimal.hpp
+++ include/decimal.hpp
@@ -18,24 +18,16 @@ using integer_t = int64_t;
 
 struct decimal_t final {
 
-  decimal_t() { m_chars.fill('0'); }
+  decimal_t() : m_view(c_zero_chars.data(), c_zero_chars.size()) {}
 
   template <typename S> decimal_t(S);
 
-  bool operator<(const decimal_t &) const { return false; }
-  bool operator>(const decimal_t &) const { return false; }
-  bool operator==(const decimal_t &rhs) const { return m_view == rhs.m_view; }
-  bool operator!=(const decimal_t &) const { return false; }
+  auto operator<=>(const decimal_t &rhs) const;
 
-  /*
-  bool operator<(const decimal_t &rhs) const { return m_value < rhs.m_value; }
-  bool operator>(const decimal_t &rhs) const { return m_value > rhs.m_value; }
-  bool operator==(const decimal_t &rhs) const { return m_value == rhs.m_value; }
-  bool operator!=(const decimal_t &rhs) const { return m_value != rhs.m_value; }
-  wide_float_t value() const { return m_value; }
-  */
-
-  //  double value() const { return 0.; }
+  bool operator<(const decimal_t &rhs) const;
+  bool operator>(const decimal_t &rhs) const;
+  bool operator==(const decimal_t &rhs) const;
+  bool operator!=(const decimal_t &rhs) const;
 
   double double_value(integer_t precision) const {
     return std::strtod(str(precision).c_str(), nullptr);
@@ -47,12 +39,19 @@ struct decimal_t final {
   void process(boost::crc_32_type &crc32, integer_t precision) const;
 
 private:
+  std::string_view int_part() const;
+  std::string_view frac_part() const;
+
+  bool is_zero() const;
+
   std::string_view m_view;
 
   static constexpr size_t c_max_num_chars =
       c_expected_cacheline_size - sizeof(m_view);
 
   std::array<char, c_max_num_chars> m_chars;
+
+  static constexpr std::array<char, 3> c_zero_chars = {'0', '.', '0'};
 };
 
 static_assert(sizeof(decimal_t) == c_expected_cacheline_size);
@@ -77,12 +76,25 @@ template <typename S> decimal_t::decimal_t(S str) {
   size_t dst_idx = 0;
   state_t state = c_before_digit;
 
-  for (const char ch : str) {
+  for (size_t idx = 0; idx < str.size(); ++idx) {
+    const char ch = str[idx];
     switch (state) {
     case c_before_digit:
       if (std::isdigit(ch)) {
-        m_chars[dst_idx++] = ch;
-        state = c_before_decimal;
+        if (ch == '0') {
+          if ((str.size() - idx) > 1) {
+            if (str[idx + 1] == '.') {
+              m_chars[dst_idx++] = ch;
+              state = c_before_decimal;
+            }
+          } else {
+            m_chars[dst_idx++] = ch;
+            state = c_after_value;
+          }
+        } else {
+          m_chars[dst_idx++] = ch;
+          state = c_before_decimal;
+        }
       }
       break;
     case c_before_decimal:
@@ -110,6 +122,35 @@ template <typename S> decimal_t::decimal_t(S str) {
   }
 
   m_view = std::string_view(m_chars.data(), dst_idx);
+  if (is_zero()) {
+    m_view = std::string_view(c_zero_chars.data(), c_zero_chars.size());
+    return;
+  }
+
+  const auto idx = m_view.find('.');
+  if (idx != std::string_view::npos) {
+    auto end_idx = m_view.size() - 1;
+    while (m_view[end_idx] == '0' && end_idx >= idx) {
+      --end_idx;
+    }
+    if (m_view[end_idx] == '.') {
+      --end_idx;
+    }
+    m_view = m_view.substr(0, end_idx + 1);
+  }
+}
+
+inline std::string_view decimal_t::int_part() const {
+  const auto decimal_point = m_view.find('.');
+  return m_view.substr(0, decimal_point);
+}
+
+inline std::string_view decimal_t::frac_part() const {
+  const auto decimal_point = m_view.find('.');
+  if (decimal_point == std::string_view::npos) {
+    return std::string_view();
+  }
+  return m_view.substr(decimal_point + 1);
 }
 
 } // namespace kdr
