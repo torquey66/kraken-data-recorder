e9fcb323078ae3862c2b5f8383d9c4eacd1ee4ec
John Finley <torquey@gmail.com>
2024-08-30 11:05:48 -0500

    Improve unit tests

--- test/unit/asset_test.cpp
+++ test/unit/asset_test.cpp
@@ -1,85 +1,86 @@
 #include <doctest/doctest.h>
 
-#include <simdjson.h>
 #include <asset.hpp>
 #include <constants.hpp>
+#include <simdjson.h>
 
 using namespace kdr::model;
 
-/*
-TEST_CASE("asset_t equality") {
-  const asset_t thing1;
-  const asset_t thing2 = thing1;
-  CHECK(thing1 == thing2);
-  const asset_t thing3{thing2};
-  CHECK(thing2 == thing3);
-}
+TEST_SUITE("asset_t") {
 
-TEST_CASE("asset_t - inequality") {
-  std::optional<double_t> no_margin_rate;
-  std::optional<double_t> margin_rate_1 = std::make_optional<double>(1.1);
-  std::optional<double_t> margin_rate_2 = std::make_optional<double>(1.2);
+  TEST_CASE("equality") {
+    const asset_t thing1;
+    const asset_t thing2 = thing1;
+    CHECK(thing1 == thing2);
+    const asset_t thing3{thing2};
+    CHECK(thing2 == thing3);
+  }
 
-  CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
-                asset_status_enabled} != asset_t{false, 0.1, "thing1",
-                                                 no_margin_rate, 8, 3,
-                                                 asset_status_enabled});
+  TEST_CASE("inequality") {
+    std::optional<double_t> no_margin_rate;
+    std::optional<double_t> margin_rate_1 = std::make_optional<double>(1.1);
+    std::optional<double_t> margin_rate_2 = std::make_optional<double>(1.2);
 
-  CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
-                asset_status_enabled} != asset_t{true, 0.2, "thing1",
-                                                 no_margin_rate, 8, 3,
-                                                 asset_status_enabled});
+    CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
+                  asset_status_enabled} != asset_t{false, 0.1, "thing1",
+                                                   no_margin_rate, 8, 3,
+                                                   asset_status_enabled});
 
-  CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
-                asset_status_enabled} != asset_t{true, 0.1, "thing2",
-                                                 no_margin_rate, 8, 3,
-                                                 asset_status_enabled});
+    CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
+                  asset_status_enabled} != asset_t{true, 0.2, "thing1",
+                                                   no_margin_rate, 8, 3,
+                                                   asset_status_enabled});
 
-  CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
-                asset_status_enabled} != asset_t{true, 0.1, "thing1",
-                                                 margin_rate_1, 8, 3,
-                                                 asset_status_enabled});
+    CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
+                  asset_status_enabled} != asset_t{true, 0.1, "thing2",
+                                                   no_margin_rate, 8, 3,
+                                                   asset_status_enabled});
 
-  CHECK(
-      asset_t{true, 0.1, "thing1", margin_rate_1, 8, 3, asset_status_enabled} !=
-      asset_t{true, 0.1, "thing1", margin_rate_2, 8, 3, asset_status_enabled});
+    CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
+                  asset_status_enabled} != asset_t{true, 0.1, "thing1",
+                                                   margin_rate_1, 8, 3,
+                                                   asset_status_enabled});
 
-  CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
-                asset_status_enabled} != asset_t{true, 0.1, "thing1",
-                                                 no_margin_rate, 6, 3,
-                                                 asset_status_enabled});
+    CHECK(asset_t{true, 0.1, "thing1", margin_rate_1, 8, 3,
+                  asset_status_enabled} != asset_t{true, 0.1, "thing1",
+                                                   margin_rate_2, 8, 3,
+                                                   asset_status_enabled});
 
-  CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
-                asset_status_enabled} != asset_t{true, 0.1, "thing1",
-                                                 no_margin_rate, 8, 7,
-                                                 asset_status_enabled});
+    CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
+                  asset_status_enabled} != asset_t{true, 0.1, "thing1",
+                                                   no_margin_rate, 6, 3,
+                                                   asset_status_enabled});
 
-  CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
-                asset_status_enabled} != asset_t{true, 0.1, "thing1",
-                                                 no_margin_rate, 8, 3,
-                                                 asset_status_disabled});
-}
+    CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
+                  asset_status_enabled} != asset_t{true, 0.1, "thing1",
+                                                   no_margin_rate, 8, 7,
+                                                   asset_status_enabled});
 
-TEST_CASE("asset_t from_json") {
-  static const std::string test_str = R"RESPONSE(
+    CHECK(asset_t{true, 0.1, "thing1", no_margin_rate, 8, 3,
+                  asset_status_enabled} != asset_t{true, 0.1, "thing1",
+                                                   no_margin_rate, 8, 3,
+                                                   asset_status_disabled});
+  }
+
+  TEST_CASE("from_json") {
+    static const std::string test_str = R"RESPONSE(
   [{"borrowable":true,"collateral_value":1.0,"id":"USD","margin_rate":0.025,"precision":4,"precision_display":2,"status":"enabled"}]
   )RESPONSE";
 
-  const asset_t expected{true,
-                         1.0,
-                         "USD",
-                         std::make_optional<double>(0.025),
-                         4,
-                         2,
-                         asset_status_enabled};
+    const asset_t expected{true,
+                           1.0,
+                           "USD",
+                           std::make_optional<double>(0.025),
+                           4,
+                           2,
+                           asset_status_enabled};
 
-  simdjson::ondemand::parser parser;
-  simdjson::padded_string padded{test_str};
-  simdjson::ondemand::document doc = parser.iterate(padded);
-  for (simdjson::fallback::ondemand::object obj : doc) {
-    const asset_t actual = asset_t::from_json(obj);
-    CHECK(expected == actual);
+    simdjson::ondemand::parser parser;
+    simdjson::padded_string padded{test_str};
+    simdjson::ondemand::document doc = parser.iterate(padded);
+    for (simdjson::fallback::ondemand::object obj : doc) {
+      const asset_t actual = asset_t::from_json(obj);
+      CHECK(expected == actual);
+    }
   }
 }
-
-*/
--- test/unit/response_test.cpp
+++ test/unit/response_test.cpp
@@ -2,43 +2,46 @@
 
 #include <responses.hpp>
 
-#include <simdjson.h>
 #include <boost/json.hpp>
+#include <simdjson.h>
 
 #include <sstream>
 
-TEST_CASE("timestamp_t from_iso_8691") {
-  const auto tp_str = "2024-04-13T18:10:04.220677Z";
-  const auto micros = kdr::timestamp_t::from_iso_8601(tp_str);
-  CHECK(micros == 1713031804220677);
-}
+TEST_SUITE("response") {
 
-TEST_CASE("timestamp_t to_iso_8601") {
-  const auto micros = 1713031804220677;
-  const auto tp_str = kdr::timestamp_t::to_iso_8601(micros);
-  CHECK(tp_str == "2024-04-13T18:10:04.220677Z");
-}
+  TEST_CASE("timestamp_t from_iso_8691") {
+    const auto tp_str = "2024-04-13T18:10:04.220677Z";
+    const auto micros = kdr::timestamp_t::from_iso_8601(tp_str);
+    CHECK(micros == 1713031804220677);
+  }
 
-TEST_CASE("timestamp_t roundtrip iso8601") {
-  const auto orig_str = std::string{"2024-04-04T22:01:57.362980Z"};
-  const auto micros = kdr::timestamp_t::from_iso_8601(orig_str);
-  const auto tstamp = kdr::timestamp_t{micros};
-  const auto rt_str = tstamp.str();
-  CHECK(rt_str == orig_str);
-}
+  TEST_CASE("timestamp_t to_iso_8601") {
+    const auto micros = 1713031804220677;
+    const auto tp_str = kdr::timestamp_t::to_iso_8601(micros);
+    CHECK(tp_str == "2024-04-13T18:10:04.220677Z");
+  }
 
-TEST_CASE("book_t parse") {
-  static const std::string test_str = R"RESPONSE(
+  TEST_CASE("timestamp_t roundtrip iso8601") {
+    const auto orig_str = std::string{"2024-04-04T22:01:57.362980Z"};
+    const auto micros = kdr::timestamp_t::from_iso_8601(orig_str);
+    const auto tstamp = kdr::timestamp_t{micros};
+    const auto rt_str = tstamp.str();
+    CHECK(rt_str == orig_str);
+  }
+
+  TEST_CASE("book_t parse") {
+    static const std::string test_str = R"RESPONSE(
 {"channel":"book","data":[{"asks":[{"price":34726.4,"qty":0E0},{"price":34739.7,"qty":2.1541}],"bids":[],"checksum":4022926185,"symbol":"BTC/USD","timestamp":"2022-06-13T08:09:10.123456Z"}],"type":"update"}
   )RESPONSE";
 
-  simdjson::ondemand::parser parser;
-  simdjson::padded_string test_response{test_str};
-  simdjson::ondemand::document doc = parser.iterate(test_response);
-  const auto book = kdr::response::book_t::from_json(doc);
+    simdjson::ondemand::parser parser;
+    simdjson::padded_string test_response{test_str};
+    simdjson::ondemand::document doc = parser.iterate(test_response);
+    const auto book = kdr::response::book_t::from_json(doc);
 
-  const auto result_str = book.str(3, 8);
-  const auto result_json = boost::json::parse(result_str);
-  const auto test_json = boost::json::parse(test_str);
-  CHECK(test_json == result_json);
+    const auto result_str = book.str(3, 8);
+    const auto result_json = boost::json::parse(result_str);
+    const auto test_json = boost::json::parse(test_str);
+    CHECK(test_json == result_json);
+  }
 }
