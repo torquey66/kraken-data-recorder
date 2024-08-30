091dcb61657100ea94d9756b19eb37e7e9489869
John Finley <torquey@gmail.com>
2024-08-30 11:05:48 -0500

    Improve unit tests

--- test/unit/level_book_test.cpp
+++ test/unit/level_book_test.cpp
@@ -6,129 +6,9 @@
 
 #include <sstream>
 
-/**
- * See https://docs.kraken.com/api/docs/websocket-v2/book for examples
- */
+TEST_SUITE("level_book_t") {
 
-static const std::string example_snapshot_str = R"RESPONSE(
-{
-   "channel": "book",
-   "type": "snapshot",
-   "data": [
-      {
-         "symbol": "BTC/USD",
-         "bids": [
-            {
-               "price": 45283.5,
-               "qty": 0.10000000
-            },
-            {
-               "price": 45283.4,
-               "qty": 1.54582015
-            },
-            {
-               "price": 45282.1,
-               "qty": 0.10000000
-            },
-            {
-               "price": 45281.0,
-               "qty": 0.10000000
-            },
-            {
-               "price": 45280.3,
-               "qty": 1.54592586
-            },
-            {
-               "price": 45279.0,
-               "qty": 0.07990000
-            },
-            {
-               "price": 45277.6,
-               "qty": 0.03310103
-            },
-            {
-               "price": 45277.5,
-               "qty": 0.30000000
-            },
-            {
-               "price": 45277.3,
-               "qty": 1.54602737
-            },
-            {
-               "price": 45276.6,
-               "qty": 0.15445238
-            }
-         ],
-         "asks": [
-            {
-               "price": 45285.2,
-               "qty": 0.00100000
-            },
-            {
-               "price": 45286.4,
-               "qty": 1.54571953
-            },
-            {
-               "price": 45286.6,
-               "qty": 1.54571109
-            },
-            {
-               "price": 45289.6,
-               "qty": 1.54560911
-            },
-            {
-               "price": 45290.2,
-               "qty": 0.15890660
-            },
-            {
-               "price": 45291.8,
-               "qty": 1.54553491
-            },
-            {
-               "price": 45294.7,
-               "qty": 0.04454749
-            },
-            {
-               "price": 45296.1,
-               "qty": 0.35380000
-            },
-            {
-               "price": 45297.5,
-               "qty": 0.09945542
-            },
-            {
-               "price": 45299.5,
-               "qty": 0.18772827
-            }
-         ],
-         "checksum": 3310070434
-      }
-   ]
-}
-)RESPONSE";
-
-static const std::string example_update_str = R"RESPONSE(
-  {
-    "channel": "book",
-    "type": "update",
-    "data": [
-        {
-            "symbol": "MATIC/USD",
-            "bids": [
-                {
-                    "price": 0.5657,
-                    "qty": 1098.3947558
-                }
-            ],
-            "asks": [],
-            "checksum": 2114181697,
-            "timestamp": "2023-10-06T17:35:55.440295Z"
-        }
-    ]
-  }
-  )RESPONSE";
-
-static const std::string pair_str = R"RESPONSE(
+  static const std::string pair_str = R"RESPONSE(
 [
   {
     "recv_tm": 1719859326224708,
@@ -149,24 +29,25 @@ static const std::string pair_str = R"RESPONSE(
 ]
 )RESPONSE";
 
-static const std::string snapshot_str = R"RESPONSE(
+  static const std::string snapshot_str = R"RESPONSE(
 {"channel":"book","type":"snapshot","data":[{"symbol":"GST/USD","bids":[{"price":0.016,"qty":255965.95133811},{"price":0.015,"qty":264465.46682136},{"price":0.014,"qty":198234.50375152},{"price":0.013,"qty":263077.71115063},{"price":0.012,"qty":135283.23181445},{"price":0.011,"qty":232726.34707055},{"price":0.010,"qty":211909.56878553},{"price":0.009,"qty":16666.66666666},{"price":0.008,"qty":13600.00000000},{"price":0.007,"qty":1000.00000000}],"asks":[{"price":0.017,"qty":94510.50669693},{"price":0.018,"qty":232489.98702916},{"price":0.019,"qty":244770.01655926},{"price":0.020,"qty":103394.23779803},{"price":0.021,"qty":120226.44704447},{"price":0.022,"qty":122811.44535027},{"price":0.023,"qty":185766.68965043},{"price":0.024,"qty":95339.83830809},{"price":0.025,"qty":32960.86333331},{"price":0.026,"qty":86326.77204454}],"checksum":1931231958}]}
 )RESPONSE";
 
-TEST_CASE("book_t doc example snapshot") {
-  auto book = kdr::model::level_book_t{kdr::model::depth_10};
+  TEST_CASE("book_t doc example snapshot") {
+    auto book = kdr::model::level_book_t{kdr::model::depth_10};
 
-  simdjson::ondemand::parser parser;
+    simdjson::ondemand::parser parser;
 
-  simdjson::padded_string pair_response{pair_str};
-  simdjson::ondemand::document pair_doc = parser.iterate(pair_response);
-  for (simdjson::fallback::ondemand::object pair_obj : pair_doc) {
-    const auto pair = kdr::model::pair_t::from_json(pair_obj);
-    book.accept(pair);
+    simdjson::padded_string pair_response{pair_str};
+    simdjson::ondemand::document pair_doc = parser.iterate(pair_response);
+    for (simdjson::fallback::ondemand::object pair_obj : pair_doc) {
+      const auto pair = kdr::model::pair_t::from_json(pair_obj);
+      book.accept(pair);
+    }
+
+    simdjson::padded_string snap_response{snapshot_str};
+    simdjson::ondemand::document snap_doc = parser.iterate(snap_response);
+    const auto snap = kdr::response::book_t::from_json(snap_doc);
+    book.accept(snap);
   }
-
-  simdjson::padded_string snap_response{snapshot_str};
-  simdjson::ondemand::document snap_doc = parser.iterate(snap_response);
-  const auto snap = kdr::response::book_t::from_json(snap_doc);
-  book.accept(snap);
 }
--- test/unit/parquet_test.cpp
+++ test/unit/parquet_test.cpp
@@ -9,244 +9,248 @@
 #include <filesystem>
 #include <iostream>
 
-void dump_parquet(const std::string& filename) {
-  auto pool = arrow::default_memory_pool();
-  auto result = arrow::io::ReadableFile::Open(filename);
-  REQUIRE(result.ok());
-  std::shared_ptr<arrow::io::RandomAccessFile> input = result.ValueOrDie();
+TEST_SUITE("parquet") {
 
-  std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
-  PARQUET_THROW_NOT_OK(parquet::arrow::OpenFile(input, pool, &arrow_reader));
-
-  std::shared_ptr<arrow::Table> table;
-  PARQUET_THROW_NOT_OK(arrow_reader->ReadTable(&table));
-  std::cout << table->ToString() << std::endl;
-}
-
-struct test_output_stream_t final {
-
-  test_output_stream_t() {
-    auto result = arrow::io::FileOutputStream::Open(
-        std::filesystem::temp_directory_path() / "test.pq");
+  void dump_parquet(const std::string &filename) {
+    auto pool = arrow::default_memory_pool();
+    auto result = arrow::io::ReadableFile::Open(filename);
     REQUIRE(result.ok());
-    m_os = result.ValueOrDie();
+    std::shared_ptr<arrow::io::RandomAccessFile> input = result.ValueOrDie();
+
+    std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
+    PARQUET_THROW_NOT_OK(parquet::arrow::OpenFile(input, pool, &arrow_reader));
+
+    std::shared_ptr<arrow::Table> table;
+    PARQUET_THROW_NOT_OK(arrow_reader->ReadTable(&table));
+    std::cout << table->ToString() << std::endl;
   }
 
-  ~test_output_stream_t() {
-    REQUIRE(m_os->Close().ok());
-    REQUIRE(m_os->closed());
-  }
+  struct test_output_stream_t final {
 
-  auto operator()() { return m_os; }
-  auto operator()() const { return m_os; }
+    test_output_stream_t() {
+      auto result = arrow::io::FileOutputStream::Open(
+          std::filesystem::temp_directory_path() / "test.pq");
+      REQUIRE(result.ok());
+      m_os = result.ValueOrDie();
+    }
 
-private:
-  std::shared_ptr<arrow::io::FileOutputStream> m_os;
-};
+    ~test_output_stream_t() {
+      REQUIRE(m_os->Close().ok());
+      REQUIRE(m_os->closed());
+    }
 
-struct mock_writer_t final {
-  mock_writer_t(const arrow::Schema& schema)
-      : m_writer_props{parquet::WriterProperties::Builder()
-                           .max_row_group_length(64 * 1024)
-                           ->created_by("KDR test")
-                           ->version(parquet::ParquetVersion::PARQUET_2_6)
-                           ->data_page_version(
-                               parquet::ParquetDataPageVersion::V2)
-                           ->compression(parquet::Compression::SNAPPY)
-                           ->build()},
-        m_arrow_props{
-            parquet::ArrowWriterProperties::Builder().store_schema()->build()},
-        m_writer{parquet::arrow::FileWriter::Open(schema,
-                                                  arrow::default_memory_pool(),
-                                                  m_tos(),
-                                                  m_writer_props,
-                                                  m_arrow_props)
-                     .ValueOrDie()} {}
+    auto operator()() { return m_os; }
+    auto operator()() const { return m_os; }
 
-  const parquet::arrow::FileWriter &writer() const { return *m_writer; }
-  parquet::arrow::FileWriter &writer() { return *m_writer; }
+  private:
+    std::shared_ptr<arrow::io::FileOutputStream> m_os;
+  };
 
-  void write(const arrow::RecordBatch &batch) {
-    const auto result = m_writer->WriteRecordBatch(batch);
+  struct mock_writer_t final {
+    mock_writer_t(const arrow::Schema &schema)
+        : m_writer_props{parquet::WriterProperties::Builder()
+                             .max_row_group_length(64 * 1024)
+                             ->created_by("KDR test")
+                             ->version(parquet::ParquetVersion::PARQUET_2_6)
+                             ->data_page_version(
+                                 parquet::ParquetDataPageVersion::V2)
+                             ->compression(parquet::Compression::SNAPPY)
+                             ->build()},
+          m_arrow_props{parquet::ArrowWriterProperties::Builder()
+                            .store_schema()
+                            ->build()},
+          m_writer{parquet::arrow::FileWriter::Open(
+                       schema, arrow::default_memory_pool(), m_tos(),
+                       m_writer_props, m_arrow_props)
+                       .ValueOrDie()} {}
+
+    const parquet::arrow::FileWriter &writer() const { return *m_writer; }
+    parquet::arrow::FileWriter &writer() { return *m_writer; }
+
+    void write(const arrow::RecordBatch &batch) {
+      const auto result = m_writer->WriteRecordBatch(batch);
+      REQUIRE(result.ok());
+    }
+
+  private:
+    test_output_stream_t m_tos;
+    std::shared_ptr<parquet::WriterProperties> m_writer_props;
+    std::shared_ptr<parquet::ArrowWriterProperties> m_arrow_props;
+    std::unique_ptr<parquet::arrow::FileWriter> m_writer;
+  };
+
+  TEST_CASE("sanity check - simple scalar") {
+    const auto fv = arrow::FieldVector{
+        arrow::field("int_field", arrow::int64(), false),
+    };
+    const auto schema = std::make_shared<arrow::Schema>(fv);
+    REQUIRE(schema);
+    auto mw = mock_writer_t{*schema};
+
+    auto fld_builder = std::make_shared<arrow::Int64Builder>();
+    fld_builder->Reset();
+
+    auto result = fld_builder->Append(42);
     REQUIRE(result.ok());
+
+    std::shared_ptr<arrow::Array> fld_array;
+    result = fld_builder->Finish(&fld_array);
+    REQUIRE(result.ok());
+
+    const auto columns = std::vector<std::shared_ptr<arrow::Array>>{fld_array};
+    std::shared_ptr<arrow::RecordBatch> batch =
+        arrow::RecordBatch::Make(schema, 1, columns);
+    mw.write(*batch);
   }
 
-private:
-  test_output_stream_t m_tos;
-  std::shared_ptr<parquet::WriterProperties> m_writer_props;
-  std::shared_ptr<parquet::ArrowWriterProperties> m_arrow_props;
-  std::unique_ptr<parquet::arrow::FileWriter> m_writer;
-};
+  TEST_CASE("sanity check - two columns") {
+    const auto fv = arrow::FieldVector{
+        arrow::field("int_field", arrow::int64(), false),
+        arrow::field("str_field", arrow::utf8(), false),
+    };
+    const auto schema = std::make_shared<arrow::Schema>(fv);
+    REQUIRE(schema);
+    auto mw = mock_writer_t{*schema};
 
-TEST_CASE("sanity check - simple scalar") {
-  const auto fv = arrow::FieldVector{
-      arrow::field("int_field", arrow::int64(), false),
-  };
-  const auto schema = std::make_shared<arrow::Schema>(fv);
-  REQUIRE(schema);
-  auto mw = mock_writer_t{*schema};
+    auto int_builder = std::make_shared<arrow::Int64Builder>();
+    int_builder->Reset();
 
-  auto fld_builder = std::make_shared<arrow::Int64Builder>();
-  fld_builder->Reset();
+    auto result = int_builder->Append(42);
+    REQUIRE(result.ok());
 
-  auto result = fld_builder->Append(42);
-  REQUIRE(result.ok());
+    std::shared_ptr<arrow::Array> int_array;
+    result = int_builder->Finish(&int_array);
+    REQUIRE(result.ok());
 
-  std::shared_ptr<arrow::Array> fld_array;
-  result = fld_builder->Finish(&fld_array);
-  REQUIRE(result.ok());
+    auto str_builder = std::make_shared<arrow::StringBuilder>();
+    str_builder->Reset();
 
-  const auto columns = std::vector<std::shared_ptr<arrow::Array>>{fld_array};
-  std::shared_ptr<arrow::RecordBatch> batch =
-      arrow::RecordBatch::Make(schema, 1, columns);
-  mw.write(*batch);
-}
+    result = str_builder->Append("forty-two");
+    REQUIRE(result.ok());
 
-TEST_CASE("sanity check - two columns") {
-  const auto fv = arrow::FieldVector{
-      arrow::field("int_field", arrow::int64(), false),
-      arrow::field("str_field", arrow::utf8(), false),
-  };
-  const auto schema = std::make_shared<arrow::Schema>(fv);
-  REQUIRE(schema);
-  auto mw = mock_writer_t{*schema};
+    std::shared_ptr<arrow::Array> str_array;
+    result = str_builder->Finish(&str_array);
+    REQUIRE(result.ok());
 
-  auto int_builder = std::make_shared<arrow::Int64Builder>();
-  int_builder->Reset();
+    const auto columns =
+        std::vector<std::shared_ptr<arrow::Array>>{int_array, str_array};
+    std::shared_ptr<arrow::RecordBatch> batch =
+        arrow::RecordBatch::Make(schema, 1, columns);
+    mw.write(*batch);
+  }
 
-  auto result = int_builder->Append(42);
-  REQUIRE(result.ok());
+  TEST_CASE("sanity check - struct") {
+    const auto sfv =
+        arrow::FieldVector{arrow::field("int_field", arrow::int64(), false),
+                           arrow::field("str_field", arrow::utf8(), false)};
+    const auto sfield = arrow::struct_(sfv);
+    const auto fv = arrow::FieldVector{
+        arrow::field("struct_field", sfield, false),
+    };
+    const auto schema = std::make_shared<arrow::Schema>(fv);
+    REQUIRE(schema);
 
-  std::shared_ptr<arrow::Array> int_array;
-  result = int_builder->Finish(&int_array);
-  REQUIRE(result.ok());
+    auto mw = mock_writer_t{*schema};
 
-  auto str_builder = std::make_shared<arrow::StringBuilder>();
-  str_builder->Reset();
+    auto int_builder = std::make_shared<arrow::Int64Builder>();
+    auto result = int_builder->Append(42);
+    REQUIRE(result.ok());
 
-  result = str_builder->Append("forty-two");
-  REQUIRE(result.ok());
+    auto str_builder = std::make_shared<arrow::StringBuilder>();
+    result = str_builder->Append("forty-two");
+    REQUIRE(result.ok());
 
-  std::shared_ptr<arrow::Array> str_array;
-  result = str_builder->Finish(&str_array);
-  REQUIRE(result.ok());
+    auto struct_builder = std::make_shared<arrow::StructBuilder>(
+        sfield, arrow::default_memory_pool(),
+        std::vector<std::shared_ptr<arrow::ArrayBuilder>>{int_builder,
+                                                          str_builder});
+    PARQUET_THROW_NOT_OK(struct_builder->Append());
+    PARQUET_THROW_NOT_OK(
+        static_cast<arrow::Int64Builder *>(struct_builder->field_builder(0))
+            ->Append(42));
+    PARQUET_THROW_NOT_OK(
+        static_cast<arrow::StringBuilder *>(struct_builder->field_builder(1))
+            ->Append("forty-two"));
 
-  const auto columns =
-      std::vector<std::shared_ptr<arrow::Array>>{int_array, str_array};
-  std::shared_ptr<arrow::RecordBatch> batch =
-      arrow::RecordBatch::Make(schema, 1, columns);
-  mw.write(*batch);
-}
+    std::shared_ptr<arrow::Array> struct_array;
+    result = struct_builder->Finish(&struct_array);
+    REQUIRE(result.ok());
 
-TEST_CASE("sanity check - struct") {
-  const auto sfv =
-      arrow::FieldVector{arrow::field("int_field", arrow::int64(), false),
-                         arrow::field("str_field", arrow::utf8(), false)};
-  const auto sfield = arrow::struct_(sfv);
-  const auto fv = arrow::FieldVector{
-      arrow::field("struct_field", sfield, false),
-  };
-  const auto schema = std::make_shared<arrow::Schema>(fv);
-  REQUIRE(schema);
+    const auto columns =
+        std::vector<std::shared_ptr<arrow::Array>>{struct_array};
+    std::shared_ptr<arrow::RecordBatch> batch =
+        arrow::RecordBatch::Make(schema, 1, columns);
+    mw.write(*batch);
+  }
 
-  auto mw = mock_writer_t{*schema};
+  TEST_CASE("sanity check - list of struct") {
+    const auto fv = arrow::FieldVector{
+        arrow::field("list_field", arrow::list(arrow::int64()), false),
+    };
+    const auto schema = std::make_shared<arrow::Schema>(fv);
+    REQUIRE(schema);
 
-  auto int_builder = std::make_shared<arrow::Int64Builder>();
-  auto result = int_builder->Append(42);
-  REQUIRE(result.ok());
+    auto mw = mock_writer_t{*schema};
 
-  auto str_builder = std::make_shared<arrow::StringBuilder>();
-  result = str_builder->Append("forty-two");
-  REQUIRE(result.ok());
+    auto int_builder = std::make_shared<arrow::Int64Builder>();
+    auto list_builder = std::make_shared<arrow::ListBuilder>(
+        arrow::default_memory_pool(), int_builder);
 
-  auto struct_builder = std::make_shared<arrow::StructBuilder>(
-      sfield, arrow::default_memory_pool(),
-      std::vector<std::shared_ptr<arrow::ArrayBuilder>>{int_builder,
-                                                        str_builder});
-  PARQUET_THROW_NOT_OK(struct_builder->Append());
-  PARQUET_THROW_NOT_OK(
-      static_cast<arrow::Int64Builder *>(struct_builder->field_builder(0))
-          ->Append(42));
-  PARQUET_THROW_NOT_OK(
-      static_cast<arrow::StringBuilder *>(struct_builder->field_builder(1))
-          ->Append("forty-two"));
+    PARQUET_THROW_NOT_OK(list_builder->Reserve(1));
+    PARQUET_THROW_NOT_OK(int_builder->Reserve(2));
 
-  std::shared_ptr<arrow::Array> struct_array;
-  result = struct_builder->Finish(&struct_array);
-  REQUIRE(result.ok());
+    PARQUET_THROW_NOT_OK(list_builder->Append(2));
+    PARQUET_THROW_NOT_OK(int_builder->Append(42));
+    PARQUET_THROW_NOT_OK(int_builder->Append(43));
 
-  const auto columns = std::vector<std::shared_ptr<arrow::Array>>{struct_array};
-  std::shared_ptr<arrow::RecordBatch> batch =
-      arrow::RecordBatch::Make(schema, 1, columns);
-  mw.write(*batch);
-}
+    std::shared_ptr<arrow::Array> result_array;
+    PARQUET_THROW_NOT_OK(list_builder->Finish(&result_array));
 
-TEST_CASE("sanity check - list of struct") {
-  const auto fv = arrow::FieldVector{
-      arrow::field("list_field", arrow::list(arrow::int64()), false),
-  };
-  const auto schema = std::make_shared<arrow::Schema>(fv);
-  REQUIRE(schema);
+    const auto columns =
+        std::vector<std::shared_ptr<arrow::Array>>{result_array};
+    std::shared_ptr<arrow::RecordBatch> batch =
+        arrow::RecordBatch::Make(schema, 1, columns);
+    mw.write(*batch);
+  }
 
-  auto mw = mock_writer_t{*schema};
+  TEST_CASE("sanity check - list of struct") {
+    const auto sfv = arrow::FieldVector{
+        arrow::field("int_field", arrow::int64(), false),
+        arrow::field("double_field", arrow::float64(), false)};
+    const auto sfield = arrow::struct_(sfv);
 
-  auto int_builder = std::make_shared<arrow::Int64Builder>();
-  auto list_builder = std::make_shared<arrow::ListBuilder>(
-      arrow::default_memory_pool(), int_builder);
+    const auto fv = arrow::FieldVector{
+        arrow::field("list_field", arrow::list(sfield), false),
+    };
+    const auto schema = std::make_shared<arrow::Schema>(fv);
+    REQUIRE(schema);
+    auto mw = mock_writer_t{*schema};
 
-  PARQUET_THROW_NOT_OK(list_builder->Reserve(1));
-  PARQUET_THROW_NOT_OK(int_builder->Reserve(2));
+    auto int_builder = std::make_shared<arrow::Int64Builder>();
+    auto double_builder = std::make_shared<arrow::DoubleBuilder>();
+    auto struct_builder = std::make_shared<arrow::StructBuilder>(
+        sfield, arrow::default_memory_pool(),
+        std::vector<std::shared_ptr<arrow::ArrayBuilder>>{int_builder,
+                                                          double_builder});
+    auto list_builder = std::make_shared<arrow::ListBuilder>(
+        arrow::default_memory_pool(), struct_builder);
 
-  PARQUET_THROW_NOT_OK(list_builder->Append(2));
-  PARQUET_THROW_NOT_OK(int_builder->Append(42));
-  PARQUET_THROW_NOT_OK(int_builder->Append(43));
+    PARQUET_THROW_NOT_OK(int_builder->Reserve(1));
+    PARQUET_THROW_NOT_OK(double_builder->Reserve(1));
+    PARQUET_THROW_NOT_OK(struct_builder->Reserve(1));
+    PARQUET_THROW_NOT_OK(list_builder->Reserve(1));
 
-  std::shared_ptr<arrow::Array> result_array;
-  PARQUET_THROW_NOT_OK(list_builder->Finish(&result_array));
+    PARQUET_THROW_NOT_OK(list_builder->Append(1));
+    PARQUET_THROW_NOT_OK(struct_builder->Append());
+    PARQUET_THROW_NOT_OK(int_builder->Append(42));
+    PARQUET_THROW_NOT_OK(double_builder->Append(42.42));
 
-  const auto columns = std::vector<std::shared_ptr<arrow::Array>>{result_array};
-  std::shared_ptr<arrow::RecordBatch> batch =
-      arrow::RecordBatch::Make(schema, 1, columns);
-  mw.write(*batch);
-}
+    std::shared_ptr<arrow::Array> list_array;
+    PARQUET_THROW_NOT_OK(list_builder->Finish(&list_array));
 
-TEST_CASE("sanity check - list of struct") {
-  const auto sfv =
-      arrow::FieldVector{arrow::field("int_field", arrow::int64(), false),
-                         arrow::field("double_field", arrow::float64(), false)};
-  const auto sfield = arrow::struct_(sfv);
-
-  const auto fv = arrow::FieldVector{
-      arrow::field("list_field", arrow::list(sfield), false),
-  };
-  const auto schema = std::make_shared<arrow::Schema>(fv);
-  REQUIRE(schema);
-  auto mw = mock_writer_t{*schema};
-
-  auto int_builder = std::make_shared<arrow::Int64Builder>();
-  auto double_builder = std::make_shared<arrow::DoubleBuilder>();
-  auto struct_builder = std::make_shared<arrow::StructBuilder>(
-      sfield, arrow::default_memory_pool(),
-      std::vector<std::shared_ptr<arrow::ArrayBuilder>>{int_builder,
-                                                        double_builder});
-  auto list_builder = std::make_shared<arrow::ListBuilder>(
-      arrow::default_memory_pool(), struct_builder);
-
-  PARQUET_THROW_NOT_OK(int_builder->Reserve(1));
-  PARQUET_THROW_NOT_OK(double_builder->Reserve(1));
-  PARQUET_THROW_NOT_OK(struct_builder->Reserve(1));
-  PARQUET_THROW_NOT_OK(list_builder->Reserve(1));
-
-  PARQUET_THROW_NOT_OK(list_builder->Append(1));
-  PARQUET_THROW_NOT_OK(struct_builder->Append());
-  PARQUET_THROW_NOT_OK(int_builder->Append(42));
-  PARQUET_THROW_NOT_OK(double_builder->Append(42.42));
-
-  std::shared_ptr<arrow::Array> list_array;
-  PARQUET_THROW_NOT_OK(list_builder->Finish(&list_array));
-
-  const auto columns = std::vector<std::shared_ptr<arrow::Array>>{list_array};
-  std::shared_ptr<arrow::RecordBatch> batch =
-      arrow::RecordBatch::Make(schema, 1, columns);
-  mw.write(*batch);
+    const auto columns = std::vector<std::shared_ptr<arrow::Array>>{list_array};
+    std::shared_ptr<arrow::RecordBatch> batch =
+        arrow::RecordBatch::Make(schema, 1, columns);
+    mw.write(*batch);
+  }
 }
