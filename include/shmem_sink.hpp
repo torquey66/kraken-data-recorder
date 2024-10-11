9a99ecb5a8b2b25444a1c229a5ef418d886a34de
John Finley <torquey@gmail.com>
2024-10-11 16:50:43 -0500

    Add trade content to shmem sink

--- include/shmem_sink.hpp
+++ include/shmem_sink.hpp
@@ -5,6 +5,7 @@
 #include "instrument.hpp"
 #include "level_book.hpp"
 #include "shmem_names.hpp"
+#include "trades.hpp"
 #include "types.hpp"
 
 #include <boost/interprocess/managed_shared_memory.hpp>
@@ -62,6 +63,28 @@ private:
 };
 
 /**
+ * Shared memory representation of trade state.
+ */
+struct trade_content_t final {
+  trade_content_t() {}
+  trade_content_t(const trade_content_t &) = default;
+  trade_content_t &operator=(const trade_content_t &) = default;
+
+  void accept(const model::trade_t &trade);
+
+  boost::json::object to_json_obj() const;
+  std::string str() const { return boost::json::serialize(to_json_obj()); }
+
+private:
+  model::ord_type_t m_ord_type = model::ord_type_invalid;
+  price_t m_price;
+  qty_t m_qty;
+  model::side_t m_side = model::side_invalid;
+  integer_t m_timestamp = 0;
+  integer_t m_trade_id = 0;
+};
+
+/**
  * RAII wrapper of named shared memory object.
  */
 struct segment_remover_t final {
@@ -96,7 +119,7 @@ private:
 };
 
 /**
- * RAII wrapper of shared memory segment, etc.
+ * RAII wrapper of book shared memory segment, etc.
  */
 struct book_segment_t final {
 
@@ -110,6 +133,25 @@ private:
   bip::managed_shared_memory m_segment;
   const std::string m_name;
   book_content_t *m_content = nullptr;
+  mutex_remover_t m_mutex_remover;
+  bip::named_mutex m_mutex;
+};
+
+/**
+ * RAII wrapper of trade shared memory segment, etc.
+ */
+struct trade_segment_t final {
+
+  trade_segment_t(const shmem_names_t &names);
+  ~trade_segment_t();
+
+  void accept(const model::trade_t &trade);
+
+private:
+  segment_remover_t m_segment_remover;
+  bip::managed_shared_memory m_segment;
+  const std::string m_name;
+  trade_content_t *m_content = nullptr;
   mutex_remover_t m_mutex_remover;
   bip::named_mutex m_mutex;
 };
@@ -131,9 +173,17 @@ struct shmem_sink_t final {
   void accept(const response::book_t &response,
               const model::level_book_t &level_book);
 
+  /**
+   * Update shared memory representation of trades.
+   */
+  void accept(const kdr::response::trades_t &response);
+
 private:
   using book_segment_ptr = std::unique_ptr<book_segment_t>;
+  using trade_segment_ptr = std::unique_ptr<trade_segment_t>;
+
   std::unordered_map<std::string, book_segment_ptr> m_book_segments;
+  std::unordered_map<std::string, trade_segment_ptr> m_trade_segments;
 };
 
 } // namespace shmem
