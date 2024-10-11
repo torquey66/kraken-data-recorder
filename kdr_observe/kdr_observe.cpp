5d96bf283efab53e2d9ccfab8f8f50ca4fca24c8
John Finley <torquey@gmail.com>
2024-10-11 17:26:07 -0500

    Include last trade in kdr_observe output

--- kdr_observe/kdr_observe.cpp
+++ kdr_observe/kdr_observe.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <vector>
 
 namespace bip = boost::interprocess;
 
@@ -21,6 +22,26 @@ void signal_handler(int signal) {
   }
 }
 
+template <typename T>
+boost::json::object observe(std::string pair, std::string kind) {
+
+  const kdr::shmem::shmem_names_t names{pair, kind};
+
+  bip::managed_shared_memory segment{bip::open_only, names.segment().c_str()};
+  T content;
+  mutex_ptr =
+      std::make_unique<bip::named_mutex>(bip::open_only, names.mutex().c_str());
+  std::pair<T *, bip::managed_shared_memory::size_type> result{
+      segment.find<T>(names.content().c_str())};
+  T *content_ptr = reinterpret_cast<T *>(result.first);
+
+  {
+    bip::scoped_lock<bip::named_mutex> lock(*mutex_ptr);
+    content = *content_ptr;
+  }
+  return content.to_json_obj();
+}
+
 int main(int argc, char *argv[]) {
   std::signal(SIGSEGV, signal_handler);
 
@@ -31,32 +52,18 @@ int main(int argc, char *argv[]) {
 
   const std::string pair{argv[1]};
 
-  using kdr::shmem::shmem_names_t;
-  const kdr::shmem::shmem_names_t names{
-      pair, std::string{shmem_names_t::c_book_kind}};
-
   try {
-    bip::managed_shared_memory segment{bip::open_only, names.segment().c_str()};
-    kdr::shmem::book_content_t content;
-    mutex_ptr = std::make_unique<bip::named_mutex>(bip::open_only,
-                                                   names.mutex().c_str());
-    std::pair<kdr::shmem::book_content_t *,
-              bip::managed_shared_memory::size_type>
-        result{
-            segment.find<kdr::shmem::book_content_t>(names.content().c_str())};
-    kdr::shmem::book_content_t *content_ptr =
-        reinterpret_cast<kdr::shmem::book_content_t *>(result.first);
+    const auto book = observe<kdr::shmem::book_content_t>(
+        pair, std::string{kdr::shmem::shmem_names_t::c_book_kind});
+    const auto trades = observe<kdr::shmem::trade_content_t>(
+        pair, std::string{kdr::shmem::shmem_names_t::c_trade_kind});
 
-    {
-      bip::scoped_lock<bip::named_mutex> lock(*mutex_ptr);
-      content = *content_ptr;
-    }
-    std::cout << content.str();
+    boost::json::object result = {{"book", book}, {"trades", trades}};
+    std::cout << boost::json::serialize(result);
 
   } catch (const std::exception &ex) {
     std::cerr << "ex: " << ex.what() << std::endl;
-    return 1;
+    return -11;
   }
-
   return 0;
 }
