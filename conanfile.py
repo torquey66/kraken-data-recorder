885caf1b3c214cb073a5b6d85e45fc02006162d7
John Finley <torquey@gmail.com>
2024-11-08 14:27:03 -0600

    Update arrow and thrift versions

--- conanfile.py
+++ conanfile.py
@@ -5,13 +5,13 @@ class KrakpotRecipe(ConanFile):
     generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
-        self.requires("arrow/16.1.0")
+        self.requires("arrow/17.0.0")
         self.requires("boost/1.85.0", force=True)
         self.requires("date/3.0.1")
         self.requires("doctest/2.4.11")
         self.requires("openssl/3.2.0")
         self.requires("simdjson/3.6.1")
-        self.requires("thrift/0.17.0")
+        self.requires("thrift/0.20.0")
 
     def configure(self):
         self.options["arrow/*"].parquet = True
